764b286d9c29a07f8ade6b0c6724dfbd2e91d4fa
Fatih SEVENCAN <121979282+FSevencan@users.noreply.github.com>
2023-10-25 21:53:05 +0300

    -
    -

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+IWorker[] workers = new IWorker[3]
+{
+    new Worker(),
+    new Manager(),
+    new Robot()
+};
+
+
+foreach (var worker in workers)
+{
+    worker.Work();
+}
+
+IEat[] eats = new IEat[2]
+{
+    new Worker(),
+    new Manager()
+};
+
+foreach (var eat in eats)
+{
+    eat.Eat();
+}
+
+interface IWorker
+{
+    void Work();
+}
+
+interface IEat
+{
+    void Eat();
+}
+interface ISalary
+{
+    void GetSalary();
+}
+
+class Manager : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+
+    }
+
+    public void GetSalary()
+    {
+        
+    }
+
+    public void Work()
+    {
+        
+    }
+}
+
+class Worker : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        
+    }
+
+    public void GetSalary()
+    {
+        
+    }
+
+    public void Work()
+    {
+        
+    }
+}
+
+class Robot : IWorker
+{
+    public void Work()
+    {
+        
+    }
+}
