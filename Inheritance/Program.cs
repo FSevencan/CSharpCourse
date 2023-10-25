5d0ee3b83bc8952d4d387d743983cab5854da6fb
Fatih SEVENCAN <121979282+FSevencan@users.noreply.github.com>
2023-10-25 21:52:17 +0300

    -
    -

---
+++ Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿Customer customer = new Customer();
+Student student = new Student();
+
+
+customer.FirstName = "Fatih";
+student.FirstName = "Ahmet";
+student.Department = "Computer Science";
+
+
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Department { get; set; }
+}
