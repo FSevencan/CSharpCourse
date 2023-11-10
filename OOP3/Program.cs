638b1db9c191b127ac19cdf0704d4f957e0429ca
Fatih SEVENCAN <121979282+FSevencan@users.noreply.github.com>
2023-11-10 11:44:57 +0300

    Yazılımcı Geliştirme Kampı 3,4,5 gün

---
+++ OOP3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using OOP3;
+
+IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+IKrediManager tasitKrediManager = new TasitKrediManager();
+IKrediManager konutKrediManager = new KonutKrediManager();
+
+BasvuruManager basvuruManager = new BasvuruManager();
+basvuruManager.BasvuruYap(ihtiyacKrediManager, new DataBaseLoggerService());
+
+ILoggerService databaseLoggerService = new DataBaseLoggerService();
+ILoggerService fileLoggerService = new FileLoggerService();
+
+
+
+List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager , tasitKrediManager};
+{
+    basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+}
